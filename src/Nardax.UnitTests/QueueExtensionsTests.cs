8e5d46562b6784ad929a43ef66a40a6f6a30d91e
trinodia <trinodia@gmail.com>
2016-09-08 21:50:32 +0200

    Added tests for queueextensions

---
+++ src/Nardax.UnitTests/QueueExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Nardax.UnitTests
+{
+    [TestClass]
+    public class QueueExtensionsTests
+    {
+        Queue<string> _queue;
+
+
+        public QueueExtensionsTests()
+        {
+            _queue = new Queue<string>();
+            _queue.Enqueue("aa");
+            _queue.Enqueue("bb");
+            _queue.Enqueue("cc");
+        }
+
+        [TestMethod]
+        public void EnqueueRange()
+        {
+            _queue.EnqueueRange(new[] { "dd", "ee" });
+
+            Assert.AreEqual("aa", _queue.Dequeue());
+            Assert.AreEqual("bb", _queue.Dequeue());
+            Assert.AreEqual("cc", _queue.Dequeue());
+            Assert.AreEqual("dd", _queue.Dequeue());
+            Assert.AreEqual("ee", _queue.Dequeue());
+        }
+    }
+}
